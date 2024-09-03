1748a1067694eb240aa4141d7cd43c37dc3ab6b2
skylarwalters <skylar_walters@brown.edu>
2024-09-03 13:55:30 -0400

    updates to assignment javascript

--- pages/assignments.js
+++ pages/assignments.js
@@ -7,6 +7,11 @@ const date_formatting = {
   hour12: true,
   minute: "2-digit",
 };
+const date_formatting_2 = {
+  weekday: "short",
+  month: "short",
+  day: "numeric",
+};
 
 const labs = [
   {
@@ -113,81 +118,81 @@ const labs = [
 const homeworks = [
   {
     title: "HW 1: Warmup",
-    out: new Date("4 Sep 2024 16:20:00 EST"),
-    due: new Date("11 Sep 2024 23:00:00 EST"),
-    late: new Date("12 Sep 2024 23:00:00 EST"),
-    link: "https://drive.google.com/file/d/1Z-dte5UflPAlq7JrSuVXmtU5hGMEsu72/view?usp=drive_link",
+    out: new Date("4 Sep 2024 15:30:00 EST"),
+    due: new Date("11 Sep 2024 22:00:00 EST"),
+    late: new Date("12 Sep 2024 22:00:00 EST"),
+    link: "https://drive.google.com/file/d/1nikYsPZNbNacVNb6DbJ_hy-7GZ-xRnLp/view?usp=drive_link",
     source: "",
   },
   {
     title: "HW 2: Procedures",
     out: new Date("12 Sep 2024 12:00:00 EST"),
-    due: new Date("18 Sep 2024 23:00:00 EST"),
-    late: new Date("19 Sep 2024 23:00:00 EST"),
+    due: new Date("18 Sep 2024 22:00:00 EST"),
+    late: new Date("19 Sep 2024 22:00:00 EST"),
     link: "",
     source: "",
   },
   {
     title: "HW 3: Recursion",
     out: new Date("19 Sep 2024 12:00:00 EST"),
-    due: new Date("25 Sep 2024 23:00:00 EST"),
-    late: new Date("26 Sep 2024 23:00:00 EST"),
+    due: new Date("25 Sep 2024 22:00:00 EST"),
+    late: new Date("26 Sep 2024 22:00:00 EST"),
     link: "",
     source: "",
   },
   {
     title: "HW 4: More Recursion",
     out: new Date("26 Sep 2024 12:00:00 EST"),
-    due: new Date("2 Oct 2024 23:00:00 EST"),
-    late: new Date("3 Oct 2024 23:00:00 EST"),
+    due: new Date("2 Oct 2024 22:00:00 EST"),
+    late: new Date("3 Oct 2024 22:00:00 EST"),
     link: "",
     source: "",
   },
   {
     title: "HW 5: More HOPs and Subsets",
     out: new Date("3 Oct 2024 12:00:00 EST"),
-    due: new Date("9 Oct 2024 23:00:00 EST"),
-    late: new Date("10 Oct 2024 23:00:00 EST"),
+    due: new Date("9 Oct 2024 22:00:00 EST"),
+    late: new Date("10 Oct 2024 22:00:00 EST"),
     link: "",
     source: "",
   },
   {
     title: "HW 6: K-Subsets and Matrices",
     out: new Date("10 Oct 2024 12:00:00 EST"),
-    due: new Date("16 Oct 2024 23:00:00 EST"),
-    late: new Date("17 Oct 2024 23:00:00 EST"),
+    due: new Date("16 Oct 2024 22:00:00 EST"),
+    late: new Date("17 Oct 2024 22:00:00 EST"),
     link: "",
     source: "",
   },
   {
     title: "HW 7: Rackettecita",
     out: new Date("17 Oct 2024 12:00:00 EST"),
-    due: new Date("23 Oct 2024 23:00:00 EST"),
-    late: new Date("24 Oct 2024 23:00:00 EST"),
+    due: new Date("23 Oct 2024 22:00:00 EST"),
+    late: new Date("24 Oct 2024 22:00:00 EST"),
     link: "",
     source: "",
   },
   {
     title: "HW 8: ADTs",
     out: new Date("24 Oct 2024 12:00:00 EST"),
-    due: new Date("30 Oct 2024 23:00:00 EST"),
-    late: new Date("31 Oct 2024 23:00:00 EST"),
+    due: new Date("30 Oct 2024 22:00:00 EST"),
+    late: new Date("31 Oct 2024 22:00:00 EST"),
     link: "",
     source: "",
   },
   {
     title: "HW 9: Trees, Trees, ...and Lists",
     out: new Date("7 Nov 2024 12:00:00 EST"),
-    due: new Date("13 Nov 2024 23:00:00 EST"),
-    late: new Date("14 Nov 2024 23:00:00 EST"),
+    due: new Date("13 Nov 2024 22:00:00 EST"),
+    late: new Date("14 Nov 2024 22:00:00 EST"),
     link: "",
     source: "",
   },
   {
     title: "HW 10: Game Tools",
     out: new Date("14 Nov 2024 12:00:00 EST"),
-    due: new Date("20 Nov 2024 23:00:00 EST"),
-    late: new Date("21 Nov 2024 23:00:00 EST"),
+    due: new Date("20 Nov 2024 22:00:00 EST"),
+    late: new Date("21 Nov 2024 22:00:00 EST"),
     link: "",
     source: "",
   },
@@ -197,8 +202,8 @@ const projects = [
   {
     title: "Bignum",
     out: new Date("23 Sep 2024 12:00:00 EST"),
-    due: new Date("4 Oct 2024 23:00:00 EST"),
-    late: new Date("5 Oct 2024 23:00:00 EST"),
+    due: new Date("4 Oct 2024 22:00:00 EST"),
+    late: new Date("5 Oct 2024 22:00:00 EST"),
     link: "",
     source: "",
     gearup: "",
@@ -206,8 +211,8 @@ const projects = [
   {
     title: "Rackette",
     out: new Date("24 Oct 2024 12:00:00 EST"),
-    due: new Date("8 Nov 2024 23:00:00 EST"),
-    late: new Date("9 Nov 2024 23:00:00 EST"),
+    due: new Date("8 Nov 2024 22:00:00 EST"),
+    late: new Date("9 Nov 2024 22:00:00 EST"),
     link: "",
     source: "",
     gearup: "",
@@ -215,8 +220,8 @@ const projects = [
   {
     title: "Game",
     out: new Date("11 Nov 2024 12:00:00 EST"),
-    due: new Date("6 Dec 2024 23:00:00 EST"),
-    late: new Date("7 Dec 2024 23:00:00 EST"),
+    due: new Date("6 Dec 2024 22:00:00 EST"),
+    late: new Date("7 Dec 2024 22:00:00 EST"),
     link: "",
     source: "",
     gearup: "",
@@ -234,57 +239,111 @@ const getAssignments = () => {
     (proj) => d.valueOf() >= proj.out.valueOf()
   );
 
-  // edit the three top cards
-  currLab = currLabs[currLabs.length - 1];
-  labCardTitle = document.getElementById("lab-assignment-card-title");
-  labCardTitle.setAttribute("href", currLab.link);
-  labCardTitle.innerHTML = currLab.title;
-  document.getElementById("lab-assignment-card-title").innerHTML =
-    currLab.title;
-  document.getElementById("lab-assignment-card-out").innerHTML =
-    currLab.out.toLocaleString("en-US", date_formatting);
-  document.getElementById("lab-assignment-card-due").innerHTML =
-    currLab.due.toLocaleString("en-US", date_formatting);
+  // update current assignment cards!
+  /*  
+    labs contain:
+      link to handout, link to slideshow, source code
+  */
+  const labCard = document.getElementById("lab-assignment-card");
+  if(currLabs.length == 0){
+    labCard.style.display = "none";
+  } else {
+    currLab = currLabs[currLabs.length - 1];
+    labCardTitle = document.getElementById("lab-assignment-card-title");
+    labCardTitle.setAttribute("href", currLab.link);
+    labCardTitle.innerHTML = currLab.title;
+    document.getElementById("lab-assignment-card-title").innerHTML =
+      currLab.title;
+    document.getElementById("lab-assignment-card-out").innerHTML =
+      currLab.out.toLocaleString("en-US", date_formatting);
+    document.getElementById("lab-assignment-card-due").innerHTML =
+     currLab.due.toLocaleString("en-US", date_formatting);
+    
+     /* add slide and source code link if present */
+    if(currLab.source == null || currLab.slide == ""){
+      document.getElementById("slide").style.display = "none";
+    }else{
+      document.getElementById("slide").setAttribute("href", currLab.slide);
+    }
+    if(currLab.source == null || currLab.source == ""){
+      document.getElementById("setup").style.display = "none";
+    }else{
+      document.getElementById("setup").setAttribute("href", currLab.source);
+    }
+  }
 
-  currHw = currHws[currHws.length - 1];
-  hwCardTitle = document.getElementById("hw-assignment-card-title");
-  hwCardTitle.setAttribute("href", currHw.link);
-  hwCardTitle.innerHTML = currHw.title;
-  document.getElementById("hw-assignment-card-out").innerHTML =
-    currHw.out.toLocaleString("en-US", date_formatting);
-  document.getElementById("hw-assignment-card-due").innerHTML =
-    currHw.due.toLocaleString("en-US", date_formatting);
+  // update hw cards
+  const hwCard = document.getElementById("hw-assignment-card");
+  if(currHws.length == 0){
+    hwCard.style.display = "none";
+  } else {
+    currHw = currHws[currHws.length - 1];
+    hwCardTitle = document.getElementById("hw-assignment-card-title");
+    hwCardTitle.setAttribute("href", currHw.link);
+    hwCardTitle.innerHTML = currHw.title;
+    document.getElementById("hw-assignment-card-out").innerHTML =
+      currHw.out.toLocaleString("en-US", date_formatting);
+    document.getElementById("hw-assignment-card-due").innerHTML =
+      currHw.due.toLocaleString("en-US", date_formatting);
+    
+    if(currHw.source == null || currHw.source == ""){
+      document.getElementById("hw-setup").style.display = "none";
+    }else{
+      document.getElementById("hw-setup").setAttribute("href", currHw.source);
+    }  
+  }
+  const projCard = document.getElementById("proj-assignment-card");
+  if(currProjs.length == 0){
+    projCard.style.display = "none";
+  } else {
+    currProj = currProjs[currProjs.length - 1];
+    projCardTitle = title = document.getElementById("proj-assignment-card-title");
+    projCardTitle.setAttribute("href", currProj.link);
+    projCardTitle.innerHTML = currProj.title;
+    document.getElementById("proj-assignment-card-out").innerHTML =
+      currProj.out.toLocaleString("en-US", date_formatting);
+    document.getElementById("proj-assignment-card-due").innerHTML =
+      currProj.due.toLocaleString("en-US", date_formatting);
 
-  currProj = currProjs[currProjs.length - 1];
-  projCardTitle = title = document.getElementById("proj-assignment-card-title");
-  projCardTitle.setAttribute("href", currProj.link);
-  projCardTitle.innerHTML = currProj.title;
-  document.getElementById("proj-assignment-card-out").innerHTML =
-    currProj.out.toLocaleString("en-US", date_formatting);
-  document.getElementById("proj-assignment-card-due").innerHTML =
-    currProj.due.toLocaleString("en-US", date_formatting);
+    if(currProj.source == null || currProj.source == ""){
+      document.getElementById("proj-setup").style.display = "none";
+    }else{
+      document.getElementById("proj-setup").setAttribute("href", currProj.source);
+    }    
+
+    if(currProj.gearup == null || currProj.gearup == ""){
+      document.getElementById("gearup").style.display = "none";
+    }else{
+      document.getElementById("gearup").setAttribute("href", currProj.gearup);
+    }
+  }
 
   // update the table HTML
-
   var hwTable = document.getElementById("homeworks-table");
 
   var labTable = document.getElementById("labs-table");
 
   var projTable = document.getElementById("projects-table");
 
-  currHws.forEach(function (row) {
+  homeworks.forEach(function (row) {
     var newRow = document.createElement("tr");
     hwTable.appendChild(newRow);
 
     var cellTitle = document.createElement("td");
-    var cellLink = document.createElement("a");
-    cellLink.setAttribute("href", row.link);
-    cellLink.innerHTML = row.title;
-    cellTitle.appendChild(cellLink);
+  
+    if (row.link == "" || row.link == null) {
+      cellTitle.textContent = row.title;
+      cellTitle.classList.add("no-decoration");
+    } else {
+      var cellLink = document.createElement("a");
+      cellLink.setAttribute("href", row.link);
+      cellLink.innerHTML = row.title;
+      cellTitle.appendChild(cellLink);
+    }
     newRow.appendChild(cellTitle);
 
     var cellOut = document.createElement("td");
-    cellOut.textContent = row.out.toLocaleString("en-US", date_formatting);
+    cellOut.textContent = row.out.toLocaleString("en-US", date_formatting_2);
     newRow.appendChild(cellOut);
 
     var cellDue = document.createElement("td");
@@ -296,43 +355,60 @@ const getAssignments = () => {
     newRow.appendChild(cellLate);
   });
 
-  currLabs.forEach(function (row) {
+  labs.forEach(function (row) {
     var newRow = document.createElement("tr");
-
-    var cellTitle = document.createElement("td");
-    var cellLink = document.createElement("a");
-    cellLink.setAttribute("href", row.link);
-    cellLink.innerHTML = row.title;
-    cellTitle.appendChild(cellLink);
-    newRow.appendChild(cellTitle);
-
-    var cellOut = document.createElement("td");
-    cellOut.textContent = row.out.toLocaleString("en-US", date_formatting);
-    newRow.appendChild(cellOut);
-
-    var cellDue = document.createElement("td");
-    cellDue.textContent = row.due.toLocaleString("en-US", date_formatting);
-    newRow.appendChild(cellDue);
-
-    var cellLate = document.createElement("td");
-    cellLate.textContent = row.late.toLocaleString("en-US", date_formatting);
-    newRow.appendChild(cellLate);
-
     labTable.appendChild(newRow);
-  });
-
-  currProjs.forEach(function (row) {
-    var newRow = document.createElement("tr");
 
     var cellTitle = document.createElement("td");
-    var cellLink = document.createElement("a");
-    cellLink.setAttribute("href", row.link);
-    cellLink.innerHTML = row.title;
-    cellTitle.appendChild(cellLink);
+  
+    if (row.link == "" || row.link == null) {
+      cellTitle.textContent = row.title;
+      cellTitle.classList.add("no-decoration");
+    } else {
+      var cellLink = document.createElement("a");
+      cellLink.setAttribute("href", row.link);
+      cellLink.innerHTML = row.title;
+      cellTitle.appendChild(cellLink);
+    }
     newRow.appendChild(cellTitle);
 
     var cellOut = document.createElement("td");
-    cellOut.textContent = row.out.toLocaleString("en-US", date_formatting);
+    cellOut.textContent = row.out.toLocaleString("en-US", date_formatting_2);
+    newRow.appendChild(cellOut);
+
+    var slideshows = document.createElement("td");
+
+    if (row.slide == "" || row.slide == null) {
+      slideshows.textContent = "";
+    } else {
+      var cellLink = document.createElement("a");
+      cellLink.setAttribute("href", row.slide);
+      cellLink.textContent = "Slides";
+      slideshows.appendChild(cellLink);
+    }
+    newRow.appendChild(slideshows);
+  });
+
+  projects.forEach(function (row) {
+    var newRow = document.createElement("tr");
+    projTable.appendChild(newRow);
+
+    var cellTitle = document.createElement("td");
+  
+    if (row.link == "" || row.link == null) {
+      cellTitle.textContent = row.title;
+      cellTitle.classList.add("no-decoration");
+    } else {
+      var cellLink = document.createElement("a");
+      cellLink.setAttribute("href", row.link);
+      cellLink.innerHTML = row.title;
+      cellTitle.appendChild(cellLink);
+    }
+    newRow.appendChild(cellTitle);
+
+    // add due dates 
+    var cellOut = document.createElement("td");
+    cellOut.textContent = row.out.toLocaleString("en-US", date_formatting_2);
     newRow.appendChild(cellOut);
 
     var cellDue = document.createElement("td");
@@ -343,6 +419,18 @@ const getAssignments = () => {
     cellLate.textContent = row.late.toLocaleString("en-US", date_formatting);
     newRow.appendChild(cellLate);
 
-    projTable.appendChild(newRow);
+    var slideshows = document.createElement("td");
+
+    if (row.gearup == "" || row.gearup == null) {
+      slideshows.textContent = "";
+    } else {
+      var cellLink = document.createElement("a");
+      cellLink.setAttribute("href", row.gearup);
+      cellLink.textContent = "Gearup";
+      slideshows.appendChild(cellLink);
+    }
+
+    newRow.appendChild(slideshows);
   });
+
 };
