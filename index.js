7863b920ad8f78f58d4691408a738594254564d5
skylarwalters <skylar_walters@brown.edu>
2024-09-05 13:25:51 -0400

    iframe adjustments

--- index.js
+++ index.js
@@ -53,6 +53,25 @@ document.addEventListener("DOMContentLoaded", () => {
   const staffCards = document.querySelectorAll("#staff-card");
   const staffFeature = document.getElementById("staff-feature");
 
+  const staffIframeSources = {
+    "Prof. Klein": "",
+    "Kristen": "https://open.spotify.com/embed/track/2LlOeW5rVcvl3QcPNPcDus?utm_source=generator",
+    "Melinda":"https://open.spotify.com/embed/track/5Cc0Pe8p7c2OJxMj07ywvc?utm_source=generator",
+    "Skylar":"https://open.spotify.com/embed/track/2KbP76N9Fw4MWKIjYyBlbC?utm_source=generator&theme=0",
+    "Catherine":"https://open.spotify.com/embed/track/6UFivO2zqqPFPoQYsEMuCc?utm_source=generator",
+    "Ella":"https://open.spotify.com/embed/track/5zcoYCmeIL392as1bwIYce?utm_source=generator",
+    "Gunner":"https://open.spotify.com/embed/track/4cBFjnFYjycw2EDWwZqdAP?utm_source=generator",
+    "Julie":"https://open.spotify.com/embed/track/2SPbioo65CuUB3H0aW1ID5?utm_source=generator",
+    "Michael":"https://open.spotify.com/embed/track/6PCUP3dWmTjcTtXY02oFdT?utm_source=generator",
+    "Nathan":"https://open.spotify.com/embed/track/1H5IfYyIIAlgDX8zguUzns?utm_source=generator",
+    "Oliver":"https://open.spotify.com/embed/track/7G3lxTsMfSx4yarMkfgnTC?utm_source=generator",
+    "Patrick":"https://open.spotify.com/embed/track/5CU1bbfY1wUPm7fU255Dhf?utm_source=generator",
+    "Sean":"https://open.spotify.com/embed/track/3gFQOMoUwlR6aUZj81gCzu?utm_source=generator",
+    "Stella":"https://open.spotify.com/embed/track/4F7SWwVXyvGlCs9TtXJbUd?utm_source=generator",
+    "Taha":"https://open.spotify.com/embed/track/1Ehdm1PDlKrdfyBsjwEvd1?utm_source=generator",
+    // Add more staff members and their iframe sources here
+  };
+
   staffCards.forEach((card) => {
     card.addEventListener("click", () => {
       const imgSrc = card.querySelector("img").src;
@@ -62,15 +81,16 @@ document.addEventListener("DOMContentLoaded", () => {
       const bio = card.querySelector("p")
         ? card.querySelector("p").textContent.trim()
         : "";
-      const song = card.querySelector("iframe");
+      //const song = card.querySelector("iframe");
 
+      songSrc = staffIframeSources[name]
       staffFeature.innerHTML = `
                 <img src="${imgSrc}" alt="${name}">
                 <h1>${name}</h1>
                 <h3>${role}</h3>
                 <h4>${info}</h4>
                 <p>${bio}</p>
-                <iframe src="${song.src}" border: none;"></iframe>
+                <iframe src="${songSrc}" border: none;"></iframe>
 
             `;
     });
