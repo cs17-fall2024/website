755ae1031b13789d050b083098f42a629a0e2df5
skylarwalters <skylar_walters@brown.edu>
2024-09-08 14:18:41 -0400

    hope

--- pages/assignments.js
+++ pages/assignments.js
@@ -16,7 +16,7 @@ const date_formatting_2 = {
 const labs = [
   {
     title: "Lab 1: Setup",
-    out: new Date("8 Sep 2024 09:00:00 EST"),
+    out: new Date("8 Sep 2024 00:00:00 EST"),
     due: new Date("15 Sep 2024 13:59:00 EST"),
     link: "https://drive.google.com/file/d/1O49c2261yas4Wl6757DRd3pZGPCemoff/view?usp=sharing",
     slide: "https://docs.google.com/presentation/d/1AYjTXpnpxLMgm5M2v_n9m9gcoL1er3BSJ2vqsD6xJs4/edit?usp=sharing",
